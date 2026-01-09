4696ca840513a13386a4d533c4613dc7e22650b2
Odair Silva Carvalho <37254416+Yodaime@users.noreply.github.com>
2026-01-08 23:21:52 -0300

    Update vite.config.ts
    ajuste onrender

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    allowedHosts: [
-      'closer-flow.onrender.com'
-    ],
+    allowedHosts: 'all',
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
